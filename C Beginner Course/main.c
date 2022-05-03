92c05fc0ebdb2e28bf17cb66a104201d4349095e
Vitor Hugo <vitor.h8@icloud.com>
2022-05-03 20:17:35 -0300

    Initial Commit

---
+++ C Beginner Course/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Beginner Course
+//
+//  Created by Vitor Hugo on 03/05/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
