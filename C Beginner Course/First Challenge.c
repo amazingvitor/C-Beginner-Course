43213fb87ed32e85d8f156ffd517bff3f688087a
Vitor Hugo <vitor.h8@icloud.com>
2022-05-03 21:15:07 -0300

    First Challenge

---
+++ C Beginner Course/First Challenge.c
@@ -0,0 +1,13 @@
+//
+//  First Challenge.c
+//  C Beginner Course
+//
+//  Created by Vitor Hugo on 03/05/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hi, my name is Vitor. \n");
+    return 0;
+}
